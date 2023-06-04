26366f489b66eaf4ed2fe4bf3dfc05dbc9c594ab
Mihai Balan <mihai.balan@esolutions.ro>
2023-06-04 20:10:16 +0300

    ?

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -9,11 +9,11 @@ const About = () => {
   return (
     <div className={style.container}>
       <div className={style.containerLeft}>
-        <div className={style.title}>Hi! I'm Cracklez :)</div>
+        <div className={style.title}>Hi! I am Cracklez :)</div>
         <div className={style.description}>
-          I`m a freelance illustrator and graphic designer based in Bucharest,
+          I am a freelance illustrator and graphic designer based in Bucharest,
           Romania. Most of my work is inspired by robots, old animes, skate-punk
-          culture, music, cartoons, toys and video games. When I`m not
+          culture, music, cartoons, toys and video games. When I am not
           drawing(very rare), I like to go outside and skate, watch movies or
           play video games with my friends.
         </div>
