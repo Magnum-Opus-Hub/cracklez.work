9bd094c852a36b353b38b4d9a6221f18f2293d64
Mihai Balan <mihai.balan@esolutions.ro>
2023-06-04 21:53:11 +0300

    ok..

--- src/pages/[id].jsx
+++ src/pages/[id].jsx
@@ -5,7 +5,6 @@ import React, { useEffect, useState } from 'react';
 import projects from '../utils/projects';
 import styles from '../styles/work.module.scss';
 import useIsMobile from "../hooks/useIsMobile";
-import { Router, useNavigate } from "react-router-dom"
 
  const WorkDetail = () => {
     const router = useRouter();
