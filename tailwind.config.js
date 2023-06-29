40d327ac5bdf4f2174bac44afface4f82d260405
FilipTheGod <filipstan666@gmail.com>
2023-06-29 16:47:37 +0300

    video done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        //we want the video that is 4 seconds long to slowly fade in and out for 4 seconds
+        "fade-in-out": {
+          "0%": {
+            opacity: "0",
+      },
+      "50%": {
+        opacity: "1",
+      },
+      "100%": {
+        opacity: "0",
+      },
+    },
+  },
+      animation: {
+        "fade-in-out": "fade-in-out 4s ease-in-out infinite",
+
+
+    },
   },
   plugins: [],
+}
 }
