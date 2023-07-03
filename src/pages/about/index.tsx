e2a02759a01df2e2e93d854d910f77c014d50b56
Mihai Balan <mihai.balan@esolutions.ro>
2023-07-03 12:46:32 +0300

    + nav + about modif

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -44,7 +44,7 @@ const About = () => {
               I am a freelance illustrator and graphic designer based in
               Bucharest, Romania. Most of my work is inspired by robots, old
               animes, skate-punk culture, music, cartoons, toys and video games.
-              When I am not drawing(very rare), I like to go outside and skate,
+              When I am not drawing, I like to go outside and skate,
               watch movies or play video games with my friends.
             </div>
             <div className={style.socialContainer}>
