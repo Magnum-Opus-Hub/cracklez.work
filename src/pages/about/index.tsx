8752d53453c2b9f22fb769f7401c91875f376678
Mihai Balan <mihai.balan@esolutions.ro>
2023-09-20 12:22:01 +0300

    + discord id

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -13,22 +13,42 @@ const About = () => {
             <div className={style.title}>Hi! I am Cracklez :)</div>
             <img src="/images/cracklez.png" alt="cracklez" />
             <div className={style.description}>
-            I'm a illustrator and graphic designer based in Bucharest, Romania. I'm a 90s kid so I grew up watching all kinds of cartoons and animes which later on inspired me to draw. Most of my personal work is inspired by robots, 90s animes, t-shirt graphics, skate culture, music, cartoons, toys and video games. Clients usually describe my work to be "nostalgic" and "eye catching".
+              I'm a illustrator and graphic designer based in Bucharest,
+              Romania. I'm a 90s kid so I grew up watching all kinds of cartoons
+              and animes which later on inspired me to draw. Most of my personal
+              work is inspired by robots, 90s animes, t-shirt graphics, skate
+              culture, music, cartoons, toys and video games. Clients usually
+              describe my work to be "nostalgic" and "eye catching".
             </div>
             <div className={style.socialContainer}>
               <div className={style.socialTitle}>Social</div>
               <div className={style.socialPlatforms}>
                 <div>
-                  <a target="_blank" href="https://www.instagram.com/cracklez/">Instagram</a>
+                  <a target="_blank" href="https://www.instagram.com/cracklez/">
+                    Instagram
+                  </a>
                 </div>
                 <div>
-                  <a target="_blank" href="https://www.behance.net/cracklez">Behance</a>
+                  <a target="_blank" href="https://www.behance.net/cracklez">
+                    Behance
+                  </a>
                 </div>
                 <div>
-                  <a target="_blank" href="https://twitter.com/Cracklez3https://twitter.com/Cracklez3">Twitter</a>
+                  <a
+                    target="_blank"
+                    href="https://twitter.com/Cracklez3https://twitter.com/Cracklez3"
+                  >
+                    Twitter
+                  </a>
                 </div>
                 <div>
-                  <a target="_blank" href="https://twitter.com/Cracklez3https://twitter.com/Cracklez3">Twitter</a>
+                  <a
+                    target="_blank"
+                    href="https://twitter.com/Cracklez3https://twitter.com/Cracklez3"
+                  >
+                    Discord ID
+                  </a>
+                  <span className={style.discordId}>cracklez#5813</span>
                 </div>
               </div>
             </div>
@@ -40,20 +60,43 @@ const About = () => {
           <div className={style.containerLeft}>
             <div className={style.title}>Hi! I am Cracklez :)</div>
             <div className={style.description}>
-            I'm a illustrator and graphic designer based in Bucharest, Romania. I'm a 90s kid so I grew up watching all kinds of cartoons and animes which later on inspired me to draw. Most of my personal work is inspired by robots, 90s animes, t-shirt graphics, skate culture, music, cartoons, toys and video games. Clients usually describe my work to be "nostalgic" and "eye catching".
+              I'm a illustrator and graphic designer based in Bucharest,
+              Romania. I'm a 90s kid so I grew up watching all kinds of cartoons
+              and animes which later on inspired me to draw. Most of my personal
+              work is inspired by robots, 90s animes, t-shirt graphics, skate
+              culture, music, cartoons, toys and video games. Clients usually
+              describe my work to be "nostalgic" and "eye catching".
             </div>
             <div className={style.socialContainer}>
               <div className={style.socialTitle}>Social</div>
               <div className={style.socialPlatforms}>
-              <div>
-                  <a target="_blank" href="https://www.instagram.com/cracklez/">Instagram</a>
+                <div>
+                  <a target="_blank" href="https://www.instagram.com/cracklez/">
+                    Instagram
+                  </a>
                 </div>
                 <div>
-                  <a target="_blank" href="https://www.behance.net/cracklez">Behance</a>
+                  <a target="_blank" href="https://www.behance.net/cracklez">
+                    Behance
+                  </a>
                 </div>
                 <div>
-                  <a target="_blank" href="https://twitter.com/Cracklez3https://twitter.com/Cracklez3">Twitter</a>
+                  <a
+                    target="_blank"
+                    href="https://twitter.com/Cracklez3https://twitter.com/Cracklez3"
+                  >
+                    Twitter
+                  </a>
                 </div>
+                <div>
+                  <a
+                    target="_blank"
+                    href="https://twitter.com/Cracklez3https://twitter.com/Cracklez3"
+                  >
+                    Discord ID
+                  </a>
+                </div>
+                <span className={style.discordId}>cracklez#5813</span>
               </div>
             </div>
           </div>
